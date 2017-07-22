57f0c7561fb93a6969877aa3cbe28d906a6d7f43
Wayne <wpa12@live.com>
2017-07-22 14:28:57 +0100

    second commit

--- mysite/public_html/index.php
+++ mysite/public_html/index.php
@@ -1,3 +1,19 @@
 <?php 
 echo "hello ";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wayne Anstey</title>
+</head>
+<body>
+
+	<div>
+		<div><header></header></div>
+		<div><nav></nav></div>
+		<div><aside></aside></div>
+		<div></div>
+	</div>
+	
+</body>
+</html>
