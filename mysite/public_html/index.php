2fc863405e69d9e446449dbd621b868ffd504ac2
Wayne <wpa12@live.com>
2017-07-24 01:04:00 +0100

    changed loads of stuff and cdn for materialize

--- mysite/public_html/index.php
+++ mysite/public_html/index.php
@@ -1,27 +1,33 @@
 <?php 
-echo "hello ";
+	
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Wayne Anstey</title>
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
-<body>
+<body class="grey lighten-5">
 
-	<div>
-		<div><header><h1>Wayne Anstey</h1></header></div>
-		<div>
-			<nav>
+	<div class="container">
+		<div class="row"><header><h1 class="h1">Wayne Anstey</h1></header></div>
+		<div class="row">
+			<nav class="purple darken-1 col s12 m10 l12">
 				<ul>
-					<li>About</li>
-					<li>Languages and Skillset</li>
-					<li>Clients</li>
+					<li><a class="">About</a></li>
+					<li><a class="">Languages and Skillset</a></li>
+					<li><a>Clients</a></li>
 				</ul>		
 			</nav>
 		</div>
-		<div><aside></aside></div>
-		<div></div>
+
+		<div class="card-panel blue darken-4">
+			<?php 
+
+			?> 
+		</div>
 	</div>
 
 </body>
